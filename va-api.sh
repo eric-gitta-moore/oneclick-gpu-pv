1464629f9ef70e7f4516af947ee6f766de367896
Eric Moore <EricGittaMoore@duck.com>
2025-08-09 22:11:30 +0800

    在 va-api.sh 脚本中添加安装 mesa-va-drivers 和 vainfo 命令，以确保 VA-API 驱动的正确配置，优化系统环境设置。

--- va-api.sh
+++ va-api.sh
@@ -5,6 +5,7 @@ apt install -y mesa-utils
 # if ls: cannot access '/dev/dri/card0': No such file or directory
 modprobe vgem
 
+apt install mesa-va-drivers vainfo
 echo "export LIBVA_DRIVER_NAME=d3d12" > /etc/profile.d/d3d.sh
 echo "export MESA_LOADER_DRIVER_OVERRIDE=vgem" >> /etc/profile.d/d3d.sh
 echo "export GST_VAAPI_DRM_DEVICE=/dev/dri/card0" >> /etc/profile.d/d3d.sh
